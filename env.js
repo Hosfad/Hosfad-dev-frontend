264e43f3c3c1b041b6fc306bab4848bc26a1879d
Hosfad <Firassabea1@outlook.com>
2022-09-14 19:03:27 +0200

    Updates

---
+++ env.js
@@ -0,0 +1,4 @@
+window.env = {
+  "API_DOMAIN": "http://localhost:8000/",
+  "DISCORD_REDIRECT_URL": "https://discord.com/api/oauth2/authorize?client_id=1007718141624852580&redirect_uri=http%3A%2F%2Flocalhost%3A3000&response_type=code&scope=identify"
+};
